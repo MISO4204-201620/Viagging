354ecf4891db2dd3120f4c8598edd03993b3c9ea
Karen Vega <karenvega@Karens-MBP.home>
2016-03-22 21:27:52 -0500

    se elimina clase File y atributo imagenPrincipal de la clase Alimentacion

--- viagging-core-common/src/main/java/com/viagging/core/services/CaracteristicaService.java
+++ viagging-core-common/src/main/java/com/viagging/core/services/CaracteristicaService.java
@@ -3,7 +3,6 @@ package com.viagging.core.services;
 import java.util.List;
 
 import com.viagging.core.model.Caracteristica;
-import com.viagging.rest.dto.AlojamientoDTO;
 
 
 public interface CaracteristicaService {
