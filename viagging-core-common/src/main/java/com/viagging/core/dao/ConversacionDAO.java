70d654e29c920c106fda330a5244477a2d7685ad
Yadiana Laitón <yadianalaiton@gmail.com>
2016-05-14 12:17:00 -0500

    Creación messsage api.

---
+++ viagging-core-common/src/main/java/com/viagging/core/dao/ConversacionDAO.java
@@ -0,0 +1,19 @@
+package com.viagging.core.dao;
+
+import java.util.List;
+
+import com.viagging.core.model.Conversacion;
+
+public interface ConversacionDAO {
+
+	Conversacion getConversacionById(Integer idConversacion);
+	
+	List<Conversacion> getAllConversaciones();
+	
+	List<Conversacion> getAllConversacionesByUsuario(Integer idUsuario);
+
+	Conversacion createConversacion(Conversacion conversacion);
+
+	Conversacion deleteConversacion(Integer idConversacion);
+	
+}
