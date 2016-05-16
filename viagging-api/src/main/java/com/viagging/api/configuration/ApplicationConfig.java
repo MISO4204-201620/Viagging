91b8e52d2cda0f376265de025f756fa6b418399b
Carlos Fernando Ruiz Nieto <monardo12@gmail.com>
2016-05-16 14:59:32 -0500

    Actualizar configuración viagging-api

--- viagging-api/src/main/java/com/viagging/api/configuration/ApplicationConfig.java
+++ viagging-api/src/main/java/com/viagging/api/configuration/ApplicationConfig.java
@@ -3,11 +3,9 @@ package com.viagging.api.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration("api")
-@PropertySource("classpath:api/application.properties")
 @ComponentScan
 class ApplicationConfig {
 
